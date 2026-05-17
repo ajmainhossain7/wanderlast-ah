2ebe62d7b70e5ac27cf88eca493643d7b31c3d02
ajmainhossain7 <ajmainh0@gmail.com>
2026-05-17 15:18:58 +0600

    Customize

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,8 +3,8 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
-const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("wanderlust");
+const client = new MongoClient(process.env.AUTH_DB_URI);
+const db = client.db("wanderlast");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
