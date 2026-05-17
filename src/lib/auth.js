c8d664824a1850995c06c5441fb642e7e63fa50d
ajmainhossain7 <ajmainh0@gmail.com>
2026-05-17 14:43:04 +0600

    Customize

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,27 +4,26 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("wanderlast");
+const db = client.db("wanderlust");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-
   emailAndPassword: {
     enabled: true,
   },
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
+        clientId: process.env.GOOGLE_CLIENTID,
+        clientSecret: process.env.GOOGLE_SECRET
+    }
   },
-  session: {
+  session : {
     cookieCache: {
       enabled: true,
       strategy: "jwt",
+      //max 7days
       maxAge: 7 * 24 * 60 * 60
     }
   },
